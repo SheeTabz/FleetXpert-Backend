5c5f83074e0aa343903ee7bb1932b0e260550d44
SheeTabz <wmuriithitabitha@gmail.com>
2024-02-22 20:24:35 +0300

    Seed vehicle type

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+vehicle_type = VehicleType.create(vehicle_type_name: "Bus")
+vehicle_type = VehicleType.create(vehicle_type_name: "Matatu")
+vehicle_type = VehicleType.create(vehicle_type_name: "Manyanga")
+vehicle_type = VehicleType.create(vehicle_type_name: "Mini Bus")
+
