2bc3575267ede092593ce0a1d5516a7fd69a8a92
SheeTabz <wmuriithitabitha@gmail.com>
2024-02-23 17:32:58 +0300

    Add the user information in driver and vehicle json object

--- app/serializers/vehicle_serializer.rb
+++ app/serializers/vehicle_serializer.rb
@@ -1,4 +1,5 @@
 class VehicleSerializer < ActiveModel::Serializer
   attributes :id, :vehicle_name, :license_plate, :ownership, :alert_cell, :alert_email, :registration_date, :seat_capacity
   has_one :vehicle_type
+  belongs_to :user
 end
