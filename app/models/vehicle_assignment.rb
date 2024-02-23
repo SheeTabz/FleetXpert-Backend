7458629e6734cf1042cfdcc02dfa9f44fc0a481a
SheeTabz <wmuriithitabitha@gmail.com>
2024-02-23 11:57:11 +0300

    Add  attributes validations

--- app/models/vehicle_assignment.rb
+++ app/models/vehicle_assignment.rb
@@ -2,4 +2,7 @@ class VehicleAssignment < ApplicationRecord
   belongs_to :vehicle
   belongs_to :driver
   belongs_to :route
+
+  validates :vehicle_id, uniqueness: true
+  validates :driver_id, uniqueness: true 
 end
