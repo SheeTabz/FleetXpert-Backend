281b5850c60fe4095e041801992afb2636712ad8
SheeTabz <wmuriithitabitha@gmail.com>
2024-02-24 11:39:43 +0300

    Add image to drivers json object

--- app/serializers/driver_serializer.rb
+++ app/serializers/driver_serializer.rb
@@ -1,4 +1,4 @@
 class DriverSerializer < ActiveModel::Serializer
-  attributes :id, :driver_name, :email, :mobile, :national_id, :license_no, :license_type, :license_issue_date, :date_of_birth, :address, :joining_date
+  attributes :id, :driver_name, :email, :mobile, :national_id, :license_no, :license_type, :license_issue_date, :date_of_birth, :address, :joining_date, :image
   belongs_to :user
 end
