99499cd8a987a0305baf97d9ee9ec64ec05b5a10
SheeTabz <wmuriithitabitha@gmail.com>
2024-02-24 11:38:38 +0300

    Add user action controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: %i[ show update destroy ]
+  # before_action :set_user, only: %i[ show update destroy ]
 
   # GET /users
   def index
@@ -10,22 +10,30 @@ class UsersController < ApplicationController
 
   # GET /users/1
   def show
+    @user = User.find_by(id: session[:user_id])
+
+    if @user
     render json: @user
+    else 
+      render json: {error: 'User not found'}, status: :not_found
+    end
   end
 
   # POST /users
   def create
-    @user = User.new(user_params)
+    @user = User.create(user_params)
 
-    if @user.save
-      render json: @user, status: :created, location: @user
+    if @user.valid?
+      session[:user_id] = @user.id
+      render json: user, status: :created, location: @user
     else
-      render json: @user.errors, status: :unprocessable_entity
+      render json: {errors: user.errors.full_messages}, status: :unprocessable_entity
     end
   end
 
   # PATCH/PUT /users/1
   def update
+    @user = set_user
     if @user.update(user_params)
       render json: @user
     else
@@ -35,6 +43,7 @@ class UsersController < ApplicationController
 
   # DELETE /users/1
   def destroy
+    @user = set_user
     @user.destroy
   end
 
@@ -46,6 +55,6 @@ class UsersController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def user_params
-      params.require(:user).permit(:name, :email, :password_digest)
+      params.require(:user).permit(:name, :email, :password, :password_confirmation))
     end
 end
